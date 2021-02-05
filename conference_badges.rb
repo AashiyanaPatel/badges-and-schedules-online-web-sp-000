ce6b50305046226eb034d78c27bb350776ee08b7
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 02:42:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
-def assign_rooms(speaker_lineup)
-  speakers_lineup.each_with_index.map {|speaker_lineup| "Hello", #{speaker}! You'll be assigned.
+def badge_maker(speakers)
+  return "Hello, my name is #{speaker}."
 end
+
+speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def printer(speakers)
   batch_badge_creator(speakers).each do |assignment|
