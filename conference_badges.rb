2fac336f166bc55f2277f9b64fd2a4a1a9dca351
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 02:35:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def assign_rooms(speaker_lineup)
   speakers_lineup.each_with_index.map {|speaker_lineup| "Hello, #{speaker}! You'll be assigned."
 end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |assignment|
+    puts assignment
+  end
+end
+    
