c1b50c64fc03c19a751326cedb49b0d0298101cd
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 02:55:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,3 @@
 def assign_rooms(speaker_lineup)
-  speakers_lineup.each_with_index.map {|speaker, index| "Hello"
-    
+  speakers_lineup.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to #{room[1-7]}"
+end
