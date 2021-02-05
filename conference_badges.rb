d599280c5c9828039f34c3325574a943e8cdc8c5
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 02:36:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def assign_rooms(speaker_lineup)
-  speakers_lineup.each_with_index.map {|speaker_lineup| "Hello, #{speaker}! You'll be assigned."
+  speakers_lineup.each_with_index.map {|speaker_lineup| "Hello", #{speaker}! You'll be assigned.
 end
 
 def printer(speakers)
