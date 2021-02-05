1d201d363112a0629545b54f8dcb76d83a6742eb
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 02:49:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,2 @@
-def badge_maker(speakers)
-  return "Hello, my name is #{speaker}."
-end
 
-speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
- end
-end
-
-def batch_badge_creator(speakers_lineup)
-  speakers_lineup.collect {|speakers| badge_maker(speaker)}
- end
-end
-
-def printer(speakers)
-  batch_badge_creator(speakers).each do |assignment|
-    puts assignment
-  end
-end
     
